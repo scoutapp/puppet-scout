5210545b5975908bbc4184d6065af49ff2511144
Derek Haynes <derek.haynes@gmail.com>
2013-08-07 13:38:54 -0600

    first commit

---
+++ Modulefile
@@ -0,0 +1,11 @@
+name    'scoutapp-scout'
+version '0.1.0'
+source 'UNKNOWN'
+author 'scoutapp'
+license 'Apache License, Version 2.0'
+summary 'Installs and configures the scoutapp.com monitoring agent.'
+description 'UNKNOWN'
+project_page 'UNKNOWN'
+
+## Add dependencies, if any:
+# dependency 'username/name', '>= 1.2.0'
---
+++ manifests/init.pp
@@ -0,0 +1,85 @@
+# == Class: scout
+#
+# Installs and configures the scoutapp.com monitoring agent.
+#
+# === Example
+#
+#  class { scout:
+#    key => 'SCOUT ACCOUNT KEY HERE',
+#    roles => ['app','memcached']
+#  }
+#
+# === Authors
+#
+# Scout <support@scoutapp.com> (Derek Haynes)
+#
+# === Copyright
+#
+# Copyright 2013 Derek Haynes
+#
+class scout(
+  $key,
+  $bin = inline_template("<%= Gem.bindir%>/scout"),
+  $gems = false,
+  $ensure = present,
+  $server_name = false,
+  $roles = [],
+  $http_proxy = false,
+  $https_proxy = false,
+  $user = 'scout',
+  $group = 'scout'
+) {  
+    # build the parameters for the scout command.
+    if ($server_name) {
+      $server_name_param = "--name ${server_name}"
+    }
+
+    if ($roles != []) {
+      $roles_param = inline_template("-r <%= roles.join(',')%>") 
+    }
+    
+    if ($http_proxy) {
+      $http_proxy_param = "--http-proxy ${http_proxy}"
+    }
+    
+    if ($https_proxy) {
+      $https_proxy_param = "--https-proxy ${https_proxy}"
+    }
+    
+    if ($user != 'root') {
+      group { $group:
+              ensure => present,
+              # gid => 1000
+      }
+      user { $user:
+         groups => $group,
+         gid => $group,
+         comment => 'This user was created by Puppet',
+         ensure => present,
+         managehome => true,
+         require => Group[$group]
+      }
+    }
+
+    package { "scout":
+        provider => gem,
+        ensure => latest
+    }
+ 
+    cron { "scout":
+        ensure => $ensure,
+        user => $user,
+        command => "${bin} ${key} ${server_name_param} ${roles_param} ${http_proxy_param} ${https_proxy_param}",
+        require => Package["scout"],
+        hour => "*",
+        minute => "*"
+    }
+ 
+    # install any plugin gem dependencies
+    if $gems {
+        package { $gems:
+            provider => gem,
+            ensure => latest
+        }
+    }
+}
