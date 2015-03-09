b1abf0bd4b602d87c12ee32ac38cff11176b2c16
Justice London <justice@ministrycentered.com>
2015-03-09 10:38:11 -0700

    Puppet lint fixes, rename class to match with the module name and implemented valid yaml role types

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,10 @@
-# == Class: scout
+# == Class: scoutd
 #
 # Installs and configures the scoutapp.com monitoring agent.
 #
 # === Example
 #
-#  class { scout:
+#  class { scoutd:
 #    key => 'SCOUT ACCOUNT KEY HERE',
 #    roles => ['app','memcached']
 #  }
@@ -17,34 +17,34 @@
 #
 # Copyright 2013 Derek Haynes
 #
-class scout(
+class scoutd(
   $account_key,
-  $hostname = false,
-  $display_name = false,
-  $log_file = false,
-  $ruby_path = false,
-  $environment = false,
-  $roles = false,
-  $http_proxy = false,
-  $https_proxy = false,
-  $gems = false,
-  $ensure = present,
+  $hostname      = false,
+  $display_name  = false,
+  $log_file      = false,
+  $ruby_path     = false,
+  $environment   = false,
+  $roles         = false,
+  $http_proxy    = false,
+  $https_proxy   = false,
+  $gems          = false,
+  $ensure        = present,
   $plugin_pubkey = ''
 ) {
-    case $operatingsystem {
+  case $::operatingsystem {
       'Ubuntu': {
         apt::key { 'scout':
-          key        => "BA012E5E",
+          key        => 'BA012E5E',
           key_source => 'https://archive.scoutapp.com/scout-archive.key',
         }
 
         apt::source { 'scout':
-          location     => 'https://archive.scoutapp.com',
-          include_src  => false,
-          release      => 'ubuntu',
-          repos        => 'main',
-          before       => Package['scoutd'],
-          require      => Apt::Key['scout']
+          location    => 'https://archive.scoutapp.com',
+          include_src => false,
+          release     => 'ubuntu',
+          repos       => 'main',
+          before      => Package['scoutd'],
+          require     => Apt::Key['scout']
         }
       }
       'RedHat', 'CentOS': {
@@ -56,46 +56,49 @@ class scout(
       'Fedora': {
         yumrepo { 'scout':
           baseurl => 'http://archive.scoutapp.com/fedora/$releasever/main/$basearch/',
-          gpgkey => 'https://archive.scoutapp.com/RPM-GPG-KEY-scout'
+          gpgkey  => 'https://archive.scoutapp.com/RPM-GPG-KEY-scout'
         }
+      }
+      default: {
+        fail("The operating system family ${::operatingsystem} is not supported by the puppet-scoutd module on ${::fqdn}")
       }
     }
 
     package { 'scoutd': ensure => latest }
 
     service { 'scout':
-      ensure => "running",
-      start => 'scoutctl restart',
+      ensure  => running,
+      start   => 'scoutctl restart',
       require => Package['scoutd']
     }
 
-    file { "/var/lib/scoutd":
+    file { '/var/lib/scoutd':
       ensure  => directory,
-      owner => 'scoutd',
-      group => 'scoutd',
+      owner   => 'scoutd',
+      group   => 'scoutd',
       require => Package['scoutd']
     }
 
-    file { "/var/lib/scoutd/.scout":
+    file { '/var/lib/scoutd/.scout':
       ensure  => directory,
       owner   => 'scoutd',
       group   => 'scoutd',
       require => File['/var/lib/scoutd']
     }
 
-    file { "/etc/scout/scoutd.yml":
-      ensure => present,
-      owner => 'scoutd',
-      group => 'scoutd',
+    file { '/etc/scout/scoutd.yml':
+      ensure  => present,
+      owner   => 'scoutd',
+      group   => 'scoutd',
       content => template('scout/scout.yml.erb'),
       require => Package['scoutd'],
-      notify => Service["scout"]
+      notify  => Service['scout']
     }
 
     if ($plugin_pubkey != '') {
       file { 'scout_plugin_pub_key':
         ensure  => present,
-        path    => "/var/lib/scoutd/.scout/scout_rsa.pub",
+        path    => '/var/lib/scoutd/.scout/scout_rsa.pub',
         content => $plugin_pubkey,
         owner   => 'scoutd',
         group   => 'scoutd',
